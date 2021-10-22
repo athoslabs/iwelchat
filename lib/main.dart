0073893b7484704fb115870c74a7f91fcfc57a5d
Robert Morrow <rcmorrow01@verizon.net>
2021-10-22 16:15:01 -0400

    Completed adding services

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,37 @@ import 'package:flutter/material.dart';
 // Packages
 import 'package:firebase_analytics/firebase_analytics.dart';
 
+// Services
+import './services/navigation_service.dart';
+
 // Pages
 import './pages/splash_page.dart';
 
 void main() {
   runApp(
-    SplashPage(key: UniqueKey(), onInitializationComplete: () {}),
+    SplashPage(
+        key: UniqueKey(),
+        onInitializationComplete: () {
+          runApp(
+            MainApp(),
+          );
+        }),
   );
+}
+
+class MainApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'iWel Coaching',
+      theme: ThemeData(
+        backgroundColor: Color.fromRGBO(36, 35, 49, 1.0),
+        scaffoldBackgroundColor: Color.fromRGBO(36, 35, 49, 1.0),
+        bottomNavigationBarTheme: BottomNavigationBarThemeData(
+          backgroundColor: Color.fromRGBO(30, 29, 37, 1.0),
+        ),
+      ),
+      // navigatorKey: NavigationService.navigatorKey,
+    );
+  }
 }
--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -4,8 +4,11 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:get_it/get_it.dart';
 
-// Pages
+// Services
 import '../services/navigation_service.dart';
+import '../services/media_service.dart';
+import '../services/cloud_storage_service.dart';
+import '../services/database_service.dart';
 
 class SplashPage extends StatefulWidget {
   final VoidCallback onInitializationComplete;
@@ -25,9 +28,12 @@ class _SplashPageState extends State<SplashPage> {
   @override
   void initState() {
     super.initState();
-    _setup().then(
-      (_) => widget.onInitializationComplete(),
-    );
+
+    Future.delayed(Duration(seconds: 2)).then((_) {
+      _setup().then(
+        (_) => widget.onInitializationComplete(),
+      );
+    });
   }
 
   @override
@@ -65,6 +71,18 @@ class _SplashPageState extends State<SplashPage> {
   void _registerServices() {
     GetIt.instance.registerSingleton<NavigationService>(
       NavigationService(),
+    );
+
+    GetIt.instance.registerSingleton<MediaService>(
+      MediaService(),
+    );
+
+    GetIt.instance.registerSingleton<CloudStorageService>(
+      CloudStorageService(),
+    );
+
+    GetIt.instance.registerSingleton<DatabaseService>(
+      DatabaseService(),
     );
   }
 }
