a78d56ef3b634a0251e211d09498f237c778644c
Robert Morrow <rcmorrow01@verizon.net>
2021-10-23 17:23:43 -0400

    Added database functionality

--- lib/providers/authentication_provider.dart
+++ lib/providers/authentication_provider.dart
@@ -16,6 +16,15 @@ class AuthenticationProvider extends ChangeNotifier {
     _auth = FirebaseAuth.instance;
     _navigationService = GetIt.instance.get<NavigationService>();
     _databaseService = GetIt.instance.get<DatabaseService>();
+
+    _auth.authStateChanges().listen((_user) {
+      if (_user != null) {
+        _databaseService.updateUserLastSeenTime(_user.uid);
+        _databaseService.getUser(_user.uid);
+      } else {
+        print("Not Authenticated");
+      }
+    });
   }
 
   Future<void> loginUsingEmailAndPassword(
