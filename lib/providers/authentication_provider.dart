95bd3b70c6ea6da9a160de475a0fdd6907b39776
Robert Morrow <rcmorrow01@verizon.net>
2021-10-24 16:40:48 -0400

    Completed login functionality

--- lib/providers/authentication_provider.dart
+++ lib/providers/authentication_provider.dart
@@ -2,27 +2,48 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get_it/get_it.dart';
+import 'package:iwelchat/models/chat_user.dart';
 
 // Services
 import '../services/database_service.dart';
 import '../services/navigation_service.dart';
+
+// Models
+import '../models/chat_user.dart';
 
 class AuthenticationProvider extends ChangeNotifier {
   late final FirebaseAuth _auth;
   late final NavigationService _navigationService;
   late final DatabaseService _databaseService;
 
+  late ChatUser user;
+
   AuthenticationProvider() {
     _auth = FirebaseAuth.instance;
     _navigationService = GetIt.instance.get<NavigationService>();
     _databaseService = GetIt.instance.get<DatabaseService>();
 
+    _auth.signOut();
+
     _auth.authStateChanges().listen((_user) {
       if (_user != null) {
         _databaseService.updateUserLastSeenTime(_user.uid);
-        _databaseService.getUser(_user.uid);
+        _databaseService.getUser(_user.uid).then((_snapshot) {
+          Map<String, dynamic> _userData =
+              _snapshot.data()! as Map<String, dynamic>;
+          user = ChatUser.fromJSON(
+            {
+              "uid": _user.uid,
+              "name": _userData["name"],
+              "email": _userData["email"],
+              "last_active": _userData["last_active"],
+              "image": _userData["image"],
+            },
+          );
+          _navigationService.removeAndNavigateToRoute('/home');
+        });
       } else {
-        print("Not Authenticated");
+        _navigationService.removeAndNavigateToRoute('/login');
       }
     });
   }
